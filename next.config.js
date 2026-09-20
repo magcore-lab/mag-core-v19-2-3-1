932a12b02a29d44626e6b062e1e842a73fad879c
Mag Core <achillejcristof@gmail.com>
2026-09-20 09:40:24 +0200

    Change TypeScript build error handling to false

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   transpilePackages: ['three'],
   eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
+  typescript: { ignoreBuildErrors: false },
 };
 module.exports = nextConfig;
